2e0fe6316d4db26aee776eaf5eb8c567a7616c9a
João Weverton <joao.weverton1997@gmail.com.com>
2025-06-15 14:52:49 -0300

    Migração para PostgreSQL

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Projeto-Recebimento-de-Notas-1",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "Flask==3.0.0",
+        "pandas==1.5.3",
+        "Flask-SQLAlchemy==3.1.1",
+        "Flask-Migrate==4.0.5",
+        "python-dotenv==1.0.0",
+        "numpy==1.23.5"
+    ],
+)
