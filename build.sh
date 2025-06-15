da726fb0695a888e9ecf7f4fbfdab58edf88d004
João Weverton <joao.weverton1997@gmail.com.com>
2025-06-15 15:44:03 -0300

    Migração para PostgreSQL

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@
 # exit on error
 set -o errexit
 
+# Instala a versão correta do Python primeiro
+pyenv install 3.11.9 -s
+pyenv global 3.11.9
+
+# Depois instala as dependências
 pip install --upgrade pip
 pip install -r requirements.txt
