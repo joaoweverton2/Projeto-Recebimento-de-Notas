05d6fcc336d4484394353c34b7c281933e9d0834
João Weverton <joao.weverton1997@gmail.com.com>
2025-06-15 15:53:54 -0300

    Migração para PostgreSQL

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -o errexit
 # Verifica a versão do Python
 echo "Python version: $(python 3.11.9)"
 
-# Instala dependências
+# Instala dependências com ordem específica para evitar conflitos
 pip install --upgrade pip
+pip install numpy==1.24.4  # Instala numpy primeiro
 pip install -r requirements.txt
